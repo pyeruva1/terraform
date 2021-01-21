3f806d2d0093816a75aa19086ae10beeba7aee59
pyeruva1 <pkry1@outlook.com>
2021-01-21 15:18:38 +0000

    created k8S master and slave nodes

--- main.tf
+++ main.tf
@@ -86,3 +86,8 @@ resource "aws_instance" "ec2" {
 
 }
 }
+
+output "private_ip" {
+  description = "List of private IP addresses assigned to the instances"
+  value       = "${aws_instance.ec2.*.private_ip}"
+}
